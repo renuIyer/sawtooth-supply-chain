96537e1245da9e3a4fec60fa82dc5feea5d369a4
Renu Iyer <renu_iyer@trimble.com>
2018-10-25 01:29:56 +0530

    simple ui changes

--- fish_client/src/main.js
+++ fish_client/src/main.js
@@ -50,8 +50,8 @@ const Layout = {
 
 const loggedInNav = () => {
   const links = [
-    ['/create', 'Add Fish'],
-    ['/fish', 'View Fish'],
+    ['/create', 'Add Loads'],
+    ['/fish', 'View Loads'],
     ['/agents', 'View Agents']
   ]
   return m(navigation.Navbar, {}, [
@@ -63,7 +63,7 @@ const loggedInNav = () => {
 
 const loggedOutNav = () => {
   const links = [
-    ['/fish', 'View Fish'],
+    ['/fish', 'View Loads'],
     ['/agents', 'View Agents']
   ]
   return m(navigation.Navbar, {}, [
